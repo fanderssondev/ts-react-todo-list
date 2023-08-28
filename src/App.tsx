d477a38d83baec90d35caf2205a51f3925c242b7
nanobyter <sotarfredrik@gmail.com>
2023-08-28 22:45:56 +0200

    Initial functionality and styling done

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,146 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import '../dist/css/main.css';
+import { v4 as uuid } from 'uuid';
 
-function App() {
-  const [count, setCount] = useState(0);
+type Todo = {
+  id: string;
+  title: string;
+  info: string;
+  completed: boolean;
+};
+
+const App = () => {
+  const [title, setTitle] = useState('');
+  const [info, setInfo] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([
+    {
+      id: uuid(),
+      title: 'Clean the plates',
+      info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      completed: false,
+    },
+    {
+      id: uuid(),
+      title: 'Study React',
+      info: "Here's some info about what you need to know when studying",
+      completed: false,
+    },
+    {
+      id: uuid(),
+      title: 'Shopping',
+      info: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur',
+      completed: false,
+    },
+  ]);
+
+  const handleCompleted = (id: string) => {
+    setTodos(previousTodos =>
+      previousTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+
+  const handleUpdate = (id: string) => {
+    // TODO: Implement
+    // setTodos(previousTodos =>
+    //   previousTodos.map(todo => {
+    //     if (todo.id === id) {
+    //       return { ...todo, completed: !todo.completed };
+    //     } else {
+    //       return todo;
+    //     }
+    //   })
+    // );
+  };
+
+  const handleDelete = (id: string) => {
+    setTodos(previousTodos => previousTodos.filter(todo => todo.id !== id));
+  };
+
+  const handleCreateNew = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setTodos(previousTodos => [
+      ...previousTodos,
+      {
+        id: uuid(),
+        title,
+        info,
+        completed: false,
+      },
+    ]);
+
+    setTitle('');
+    setInfo('');
+  };
 
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+      <div className='container'>
+        <h1 className='header'>Todo List</h1>
+        <form className='form' onSubmit={handleCreateNew}>
+          <div>
+            <label htmlFor='title'>Title</label>
+            <input
+              className='form-field'
+              type='text'
+              id='title'
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor='info'>Info</label>
+            <textarea
+              className='form-field'
+              id='info'
+              value={info}
+              onChange={e => setInfo(e.target.value)}
+            ></textarea>
+          </div>
+          <button className='form-button' type='submit'>
+            Create Todo
+          </button>
+        </form>
+
+        <div className='todo-list'>
+          {todos.map(todo => {
+            return (
+              <div
+                key={todo.id}
+                className={`card ${todo.completed ? 'completed' : ''}`}
+                onClick={() => handleCompleted(todo.id)}
+              >
+                <div className='card-text'>
+                  <h2 className='card-text-title'>{todo.title}</h2>
+                  <p className='card-text-info'>{todo.info}</p>
+                </div>
+                <div className='card-buttons'>
+                  <button
+                    className='card-buttons-delete'
+                    onClick={() => handleDelete(todo.id)}
+                  >
+                    &#x2716;
+                  </button>
+                  <button
+                    className='card-buttons-edit'
+                    onClick={() => handleUpdate(todo.id)}
+                  >
+                    Edit
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
-}
+};
 
 export default App;
